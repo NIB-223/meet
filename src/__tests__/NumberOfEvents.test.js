54c64081e685afd3c66d1c6a808a30cbb69bc98d
Robert Uebelhor <robertu.dev@gmail.com>
2021-10-27 12:40:29 -0500

    updated NumberOfEvents test file -updated line 8 to pass updateEvents prop

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,7 +5,7 @@ import NumberOfEvents from "../NumberOfEvents";
 describe("<NumberOfEvents /> component", () => {
     let NumberOfEventsWrapper;
     beforeAll(() => {
-        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+        NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => { }} />);
     });
 
     test("textbox is rendered", () => {
