7dcee5e7c756da0b9163dd8ce96df0c399a0e600
Robert Uebelhor <robertu.dev@gmail.com>
2021-11-02 19:42:53 -0500

    styled welcome page

---
+++ src/WelcomeScreen.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import './WelcomeScreen.css';
+import logo from './img/logo.svg';
+import googleLogo from './img/google-logo.jpg';
+function WelcomeScreen(props) {
+    return props.showWelcomeScreen ?
+        (
+            <div className="WelcomeScreen">
+                <nav className="welcome-nav">
+                     <img className="welcome-logo" src={logo} alt="logo"></img>
+                </nav>
+
+                <div className="content-container">
+                    <img  className="google-logo" src={googleLogo}></img>
+                <h4 className="welcome-text">
+                    Log in to see upcoming events around the world for
+                    full-stack
+                    developers
+                </h4>
+                <div className="button_cont" align="center">
+                    <div className="google-btn">
+                        <div className="google-icon-wrapper">
+                            <img
+                                className="google-icon"
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                                alt="Google sign-in"
+                            />
+                        </div>
+                        <button onClick={() => { props.getAccessToken() }}
+                            rel="nofollow noopener"
+                            className="btn-text"
+                        >
+                            <b>Sign in with google</b>
+                        </button>
+                    </div>
+                </div>
+                <a
+                    className="privacy-link"
+                    href="https://drxl95.github.io/meet/privacy.html"
+                    rel="nofollow noopener">
+                    Privacy policy
+                </a>
+                </div>
+        </div>
+                
+               
+        )
+        : null
+}
+export default WelcomeScreen;
